eb467fc12c7d565872e2bd64dd3dc29800d6e2b7
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-06-29 09:44:34 -0300

    added queries && IPost && post actions

--- index.d.ts
+++ index.d.ts
@@ -1,63 +1,62 @@
-export interface IReply {
-    date: string;
-    name: string;
-    country: {
-        code:string
-        name:string
-        src:string|undefined
-        custom:boolean|undefined
-    } | null;
-    board: string;
-    tripcode: string | null;
-    capcode: string | null;
-    subject: string | null;
-    message: string;
-    messagehash: string;
-    nomarkup: string;
-    thread: number | null;
-    email: string;
+export interface IFile{
+    filename: string;
     spoiler: boolean;
-    userId: string | null;
-    files: {
-        filename: string;
-        spoiler: boolean;
-        hash: string;
-        originalFilename: string;
-        mymetype: string;
-        size: number;
-        extension: string;
-        phash: string;
-        sizeString: string;
-        thumbextension: string;
-        geometry: {
-            width: number;
-            height: number;
-            thumbwidth: number;
-            thumbheight: number;
-            geometryString: string;
-            hasThumb: boolean;
-        };
-    }[] | [];
-    quotes: {
-        thread: number;
-        postId: number;
-    }[] | [];
-    crossquotes: [];
-    backlinks: {
-        postId: number;
-    }[] | [];
-    postId: number;
+    hash: string;
+    originalFilename: string;
+    mymetype: string;
+    size: number;
+    extension: string;
+    phash: string;
+    sizeString: string;
+    thumbextension: string;
+    geometry: {
+        width: number;
+        height: number;
+        thumbwidth: number;
+        thumbheight: number;
+        geometryString: string;
+        hasThumb: boolean;
+    }
 }
-export interface IThread extends IReply {
-    thread: null;
-    replyposts: number;
-    replyfiles: number;
-    sticky: number;
-    bumplocked: number;
-    cyclic: number;
-    bumped: string;
-    replies: IReply[] | [];
+
+export interface IBoardQuery{
+    /**
+     * 	A string to search by board name and tags, including prefix matches.
+     */
+    search?:string
+    /**
+     * `popularity` sorts by a compound of (ips, pph, sequence_value), `activity` sorts by lastPostTimestamp.
+     */
+    sort?:'popularity'|'activity'
+    /**
+     * ``desc`` or ``asc`` for descending or ascending sort order respectively.
+     */
+    direction?: 'desc'|'asc'
+    /**
+     * If ``true``, put local sites grouped before webring sites. Sort and direction for local and webring sites will be independent.
+     */
+    local_first?: boolean
+    /**
+     * Sites to include by siteName, can be repeated to include multiple sites. Blank includes all sites.
+     */
+    sites?:string
+    /**
+     * Page number. 30 per page. Blank is first page. Page numbering starts at 1. Included in all responses is maxPage.
+     */
+    page?:number
 }
+
+export interface IOverboardQuery{
+    /**
+     * 	Whether to include boards that are publicly listed and by default appear on the overboard. If not ``true`` and the add_boards parameter is empty, this will be ignored and default to true (otherwise you are asking for an overboard view of no boards).
+     */
+    include_default?:boolean
+    /**  Additional boards to fetch threads from. Can be repeated for multiple boards.*/
+    add?:string
+    /** Boards to remove from the default board list if include_detault is true. Can be repeated for multiple boards. */
+    rem?:string
+}
+
 export interface INewPost {
     /**
      * Use null to create a new thread, or the thread number to create a reply
@@ -99,6 +98,57 @@ export interface INewPost {
      */
     captcha?: number[] | string;
 }
+
+export interface IPostAction{
+    /** 
+     * Array of postId's to action against.
+    */
+    checkedposts:number[]
+    /**
+     * Delete the selected posts.
+     */
+    delete?:boolean
+    /**
+     * Spoiler all files in the selected posts.
+     */
+    spoiler?:boolean
+    /**
+     * Unlink files from the selected posts. If there are other copies of the file in other posts, or the global settings do not immediately prune unlinked files, the file may remain on disk.
+     */
+    unlink_file?:boolean
+    /**
+     * Password for anonymous users, the same one used to create the posts.
+     */
+    postpassword:string
+    /**
+     * Report the selected posts to board staff.
+     */
+    report?:boolean
+    /**
+     * Report the selected posts to global staff.
+     */
+    global_report?:boolean
+    /**
+     * Reason for reporting, if report or global_report.
+     */
+    report_reason?:string
+    /**
+     * 
+     */
+    captcha:number[]|string
+}
+
+export interface IThread extends IReply {
+    thread: null;
+    replyposts: number;
+    replyfiles: number;
+    sticky: number;
+    bumplocked: number;
+    cyclic: number;
+    bumped: string;
+    replies: IReply[] | [];
+}
+
 export interface IBoard {
     _id: string;
     tags: string[];
@@ -131,4 +181,79 @@ export interface IOverboard {
 
 export interface IBoardList{
     boards:IBoard[]
+}
+
+export interface IPost{
+    date: string;
+    name: string;
+    country: {
+        code:string
+        name:string
+        src:string|undefined
+        custom:boolean|undefined
+    } | null;
+    ip?:{
+        raw:string
+        cloak:string
+    }
+    edited?:{
+        username:string
+        date:string
+    }
+    board: string;
+    tripcode: string | null;
+    capcode: string | null;
+    subject: string | null;
+    message: string;
+    messagehash: string;
+    nomarkup: string;
+    thread: number | null;
+    email: string;
+    spoiler: boolean;
+    userId: string | null;
+    files: IFile[] | [];
+    quotes: {
+        thread: number;
+        postId: number;
+    }[] | [];
+    crossquotes: [];
+    backlinks: {
+        postId: number;
+    }[] | [];
+    postId: number;
+}
+
+/**
+ * @deprecated Use IPost
+ */
+ export interface IReply {
+    date: string;
+    name: string;
+    country: {
+        code:string
+        name:string
+        src:string|undefined
+        custom:boolean|undefined
+    } | null;
+    board: string;
+    tripcode: string | null;
+    capcode: string | null;
+    subject: string | null;
+    message: string;
+    messagehash: string;
+    nomarkup: string;
+    thread: number | null;
+    email: string;
+    spoiler: boolean;
+    userId: string | null;
+    files: IFile[] | [];
+    quotes: {
+        thread: number;
+        postId: number;
+    }[] | [];
+    crossquotes: [];
+    backlinks: {
+        postId: number;
+    }[] | [];
+    postId: number;
 }
